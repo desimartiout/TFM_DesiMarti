fbf1aec85e4c2220b35a41a03710830a4a0fd2fd
desimartiout <desimarti@outlook.com>
2024-11-17 12:22:54 +0100

    prueba de evaluación de modelo con dataset en RAGAS

--- pruebas_iniciales/ragas/RAG_With_Models/prueba.py
+++ pruebas_iniciales/ragas/RAG_With_Models/prueba.py
@@ -24,6 +24,15 @@ from langchain_community.embeddings import OllamaEmbeddings
 # loading the V2 dataset
 amnesty_qa = load_dataset("explodinggradients/amnesty_qa", "english_v2",trust_remote_code=True)
 
+""" {'question': 'Which private companies in the Americas are the largest GHG emitters according to the Carbon Majors database?',
+ 'ground_truths': ['The largest private companies in the Americas that are the largest GHG emitters according to the Carbon Majors database are ExxonMobil, Chevron, and Peabody.'],
+ 'answer': 'According to the Carbon Majors database, the largest private companies in the Americas that are the largest GHG emitters are:\n\n1. Chevron Corporation (United States)\n2. ExxonMobil Corporation (United States)\n3. ConocoPhillips Company (United States)\n4. BP plc (United Kingdom, but with significant operations in the Americas)\n5. Royal Dutch Shell plc (Netherlands, but with significant operations in the Americas)\n6. Peabody Energy Corporation (United States)\n7. Duke Energy Corporation (United States)\n8. TotalEnergies SE (France, but with significant operations in the Americas)\n9. BHP Group Limited (Australia, but with significant operations in the Americas)\n10. Rio Tinto Group (United Kingdom/Australia, but with significant operations in the Americas)\n\nPlease note that the rankings may change over time as new data becomes available.',
+ 'contexts': ['The private companies responsible for the most emissions during this period, according to the database, are from the United States: ExxonMobil, Chevron and Peabody.\nThe largest emitter amongst state-owned companies in the Americas is Mexican company Pemex, followed by Venezuelan company Petróleos de Venezuela, S.A.']} """
+
+amnesty_qa['eval'].to_json("c:/Users/desim/Documents/GitHub/TFM_DesiMarti/pruebas_iniciales/ragas/RAG_With_Models/data/amnesty_qa_eval.json")
+""" amnesty_qa['validation'].to_json("/data/amnesty_qa_validation.json")
+amnesty_qa['test'].to_json("/data/amnesty_qa_test.json")
+ """
 amnesty_subset = amnesty_qa["eval"].select(range(2))
 amnesty_subset.to_pandas()
 
