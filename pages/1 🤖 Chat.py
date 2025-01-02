201be2b9b313a157c67e5d8cc1cf08db5aa5c6f8
desimartiout <desimarti@outlook.com>
2025-01-02 18:46:52 +0100

    Actualizo el prompt

--- pages/1 🤖 Chat.py
+++ pages/1 🤖 Chat.py
@@ -134,7 +134,7 @@ def render_chatbot_page() -> None:
             )
             
             logger.info("Respuesta generada y mostrada.")
-            st.toast(f"white_check_mark: Respuesta generada y mostrada correctamente") # Mensaje TOAST
+            st.toast(f":white_check_mark: Respuesta generada y mostrada correctamente") # Mensaje TOAST
 
             # sentiment_mapping = [":material/thumb_down:", ":material/thumb_up:"]
             # selected = st.feedback("thumbs")
